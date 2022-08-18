1f2baaf1039d7f7e55cad7b137ca2643fd8b9cc6
Umair Abbas <windrgames@gmail.com>
2022-08-18 21:02:12 +0500

    Updates

---
+++ precache-manifest.b6cd3f8107011ebc2ff04aeb08d2370f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e17cc4edefca044ced9b",
+    "url": "/quotegenapp/static/css/main.26477cf9.chunk.css"
+  },
+  {
+    "revision": "e17cc4edefca044ced9b",
+    "url": "/quotegenapp/static/js/main.e17cc4ed.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/quotegenapp/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "3f74f14483aa6d119cd4",
+    "url": "/quotegenapp/static/js/2.3f74f144.chunk.js"
+  },
+  {
+    "revision": "4afd38f1c4cbddd9f57e",
+    "url": "/quotegenapp/static/js/runtime~main.4afd38f1.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/quotegenapp/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "00f0ada637a5ad8a53653f4ba6fadd52",
+    "url": "/quotegenapp/index.html"
+  }
+];
